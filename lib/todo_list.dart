92d556ea7fee7fe28acecaa9f10e2cd14c43d734
Savita soni <savitasoni@Savitas-MacBook-Air.local>
2024-08-29 18:34:56 +0530

    first commit

---
+++ lib/todo_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:to_do_cubit/models/todo_model.dart';
+
+import 'cubit/todo_cubit.dart';
+
+class TodoList extends StatelessWidget {
+  const TodoList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Todo List'),
+      ),
+      body: BlocBuilder<TodoCubit, List<Todo>>(
+        builder: (context, todos) {
+          return ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              final todo = todos[index];
+
+              return ListTile(
+                title: Text(todo.name),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, '/add-todo');
+        },
+        tooltip: 'Add Todo',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
